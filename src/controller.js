088ba67b4d970bb74e913400ccd08b285f2ff722
marcosjavier <javierojeda21@gmail.com>
2024-08-14 22:50:13 -0300

    agregado routes y funcion add en controller.js

---
+++ src/controller.js
@@ -0,0 +1,33 @@
+import {pool} from './database.js';
+
+class LibroController{
+
+  async getAll(req, res) {
+    const [result] = await pool.query('SELECT * FROM libros');
+    res.json(result);  
+  }
+
+  async add(req, res){
+    try {
+        /*capturamos el 'cuerpo' de la solicitud http de la solicitud enviada
+        por el cliente y la guardamos en la constante persona para después realizar
+        una sentencia INSERT INTO SQL */
+        //console.log('EY!!'+req.body);
+        const libro = req.body;       
+
+        const [result] = await pool.query
+        (
+          `INSERT INTO libros(nombre, autor, categoria, anio_publicacion, ISBN)VALUES(?, ?, ?, ?, ?)`,
+          [
+            libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN
+          ]
+        );
+        res.json({"Libro Insertado": result.insertId});
+      } catch (error) {
+        res.status(500).json({ error: 'Error al insertar libro', details: error.message }); 
+    }
+  }
+
+}
+
+export const libro = new LibroController();
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 
+//importamos las rutas que exportamos en nuestro archivo routes.js
+import { router } from "./routes.js";
+
 //crea el servidor de la api rest
 const app = express();
 
 //seteamos el puerto para escuchar las solicitudes
 app.set('port', 3000);
+//interpreta los objetos JSON de las solicitudes de los clientes que se van enviando
+//esta línea tiene que ir sí o si antes de la línea de código de morgan
+app.use(express.json());
 
 //para poder ver la solicitud de los clientes
 app.use(morgan('dev'));
 
-//interpreta los objetos JSON de las solicitudes de los clientes que se van enviando
-app.use(express.json());
+app.use(router);
 
 //levantamos el servicio indicandole a nuestra aplicacion cuál es el puerto, el cual
 //ya habíamos seteado en 3000 así que vá estar escuchando en ese puerto
